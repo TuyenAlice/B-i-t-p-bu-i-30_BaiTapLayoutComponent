6bced7c4b6f56ea188409055e6dada623ddc2e2f
TuyenAlice <tuyenalice77@gmail.com>
2024-09-14 20:04:27 +0700

    push again

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { BaiTapThucHanh } from "./BaiTapComponent/BaiTapThucHanh";
+function App() {
+  return (
+    <div className="App">
+      <BaiTapThucHanh />
+    </div>
+  );
+}
+
+export default App;
